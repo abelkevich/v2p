16ff85f07b7d030b0d5ae5595885d375ab2d07b9
abelkevich <aleksey.belkevich@gmail.com>
2020-07-19 09:55:56 -0400

    midi writer refactor

--- midi_writer.c
+++ midi_writer.c
@@ -4,10 +4,10 @@
 struct MidiHeader
 {
     char mthd[4];
-    char header_len[4];
-    char format[2];
-    char n[2];
-    char div[2];
+    uint32_t header_len;
+    uint16_t format;
+    uint16_t n;
+    uint16_t div;
 };
 #pragma pack(pop)
 
@@ -15,7 +15,7 @@ struct MidiHeader
 struct TrackHeader
 {
     char mtrk[4];
-    char data_len[4];
+    uint32_t data_len;
 };
 #pragma pack(pop)
 
@@ -32,45 +32,79 @@ typedef struct MidiHeader MidiHeader;
 typedef struct TrackHeader TrackHeader;
 typedef struct EventNoteOnOff EventNoteOnOff;
 
-char c_note_on = 0b1001;
-char c_note_off = 0b1000;
+const uint8_t c_midi_header_size = 14;
+const uint8_t c_track_header_size = 8;
+
+static uint16_t Reverse16(uint16_t value)
+{
+    return (((value & 0x00FF) << 8) |
+            ((value & 0xFF00) >> 8));
+}
+    
+static uint32_t Reverse32(uint32_t value) 
+{
+    return (((value & 0x000000FF) << 24) |
+            ((value & 0x0000FF00) <<  8) |
+            ((value & 0x00FF0000) >>  8) |
+            ((value & 0xFF000000) >> 24));
+}
+
+static void write_delta_time(char** data_offset, uint8_t ticks)
+{
+    **data_offset = ticks; // delta time
+    *data_offset += 1;
+}
+
+static void write_note_on_off_status(char** data_offset, const uint8_t midi_note, const uint8_t velocity, const uint8_t is_on)
+{
+    static char c_note_on =  0b10010000 | 0b0000; // add channel as last 4 bits
+    static char c_note_off = 0b10000000 | 0b0000;
+
+    EventNoteOnOff event = {0, 0, 0};
+    event.on_off_channel = is_on ? c_note_on : c_note_off;
+    event.key = midi_note;
+    event.velocity = velocity;
+
+    memcpy(*data_offset, &event, sizeof(EventNoteOnOff));
+
+    *data_offset += sizeof(EventNoteOnOff);
+}
 
 char* generate_midi_file(const NoteRec* notes, const uint32_t notes_n, uint32_t *midi_file_size)
 {
-    char* const data = (char*) malloc(3000);
+    char* const data = (char*) malloc(8 * notes_n + sizeof(MidiHeader) + sizeof(TrackHeader));
     char* data_offset = data;
 
-    static const MidiHeader midi_header = {{'M', 'T', 'h', 'd'}, {0x00, 0x00, 0x00, 0x06}, 
-                                            {0x00, 0x00}, {0x00, 0x01}, {0xE7, 0x28}};
-    memcpy(data_offset, &midi_header, sizeof(MidiHeader));
-    data_offset += sizeof(MidiHeader);
-    
-    static const TrackHeader track_header = {{'M', 'T', 'r', 'k'}, {0x00, 0x00, 0x0B, 0x88}};
-    memcpy(data_offset, &track_header, sizeof(TrackHeader));
-    data_offset += sizeof(TrackHeader);
-
-    EventNoteOnOff event_note_on_off = {0, 0, 0};
-
-    for (uint32_t midi_table_ind = 0; midi_table_ind < notes_n; midi_table_ind++)
     {
-        event_note_on_off.key = notes[midi_table_ind].semitone + 21;
-        event_note_on_off.velocity = notes[midi_table_ind].velocity * 127;
+        MidiHeader midi_header;
+        strcpy(midi_header.mthd, "MThd");
+        midi_header.header_len = Reverse32(6);
+        midi_header.format = 0;
+        midi_header.n = Reverse16(1);
+        midi_header.div = Reverse16(64);
 
-        event_note_on_off.on_off_channel = c_note_on << 4 | 0b0000;
-        
-        *data_offset = 0b00000010; // delta time
-        data_offset += 1;
+        memcpy(data_offset, &midi_header, sizeof(MidiHeader));
+        data_offset += sizeof(MidiHeader);
+    }
 
-        memcpy(data_offset, &event_note_on_off, sizeof(EventNoteOnOff));
-        data_offset += sizeof(EventNoteOnOff);
+    {
+        TrackHeader track_header;
+        strcpy(track_header.mtrk, "MTrk");
+        track_header.data_len = Reverse32(notes_n * 2 * 4);
 
-        event_note_on_off.on_off_channel = c_note_off << 4 | 0b0000;
-        
-        *data_offset = 0b00000010; // delta time
-        data_offset += 1;
+        memcpy(data_offset, &track_header, sizeof(TrackHeader));
+        data_offset += sizeof(TrackHeader);
+    }
 
-        memcpy(data_offset, &event_note_on_off, sizeof(EventNoteOnOff));
-        data_offset += sizeof(EventNoteOnOff);
+    for (uint32_t note_ind = 0; note_ind < notes_n; note_ind++)
+    {
+        uint8_t midi_note = notes[note_ind].semitone + 21;
+        uint8_t velocity = notes[note_ind].velocity * 127;
+
+        write_delta_time(&data_offset, 0);
+        write_note_on_off_status(&data_offset, midi_note, velocity, 1);
+        write_delta_time(&data_offset, 32);
+        write_note_on_off_status(&data_offset, midi_note, velocity, 0);
     }
 
     *midi_file_size = (uint32_t) (data_offset - data);
